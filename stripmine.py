23ae8be3ff38efbf2492df869379dff7c6a0faba
Jeremy T. Bouse <jbouse@debian.org>
2011-07-07 20:51:25 -0400

    Functional StripMiner ready for testing

--- stripmine.py
+++ stripmine.py
@@ -3,9 +3,8 @@
 from multiprocessing import Process
 from optparse import OptionParser
 import ConfigParser 
-
-import os, sys
-import logging as log
+import ADL
+import os, sys, logging
 import pyopencl as cl
 from struct import *
 from time import sleep, time, strftime
@@ -15,42 +14,53 @@ from BitcoinMiner import *
 
 VERSION = '2011.07.06'
 
-USER_AGENT = 'stripmine/' + VERSION
+USER_AGENT = 'stripminer/' + VERSION
+
+TIMEOUT = 10
+
+LOG_LEVEL = logging.DEBUG
+LOG_FORMAT = '%(asctime)s %(name)-10s %(levelname)-8s %(message)s'
+LOG_DATEFMT = '%Y-%m-%d %H:%M:%S'
 
 def Miner(device, options):
-	log.debug('Worker starting on device ' + device)
-	log.debug('Setting DISPLAY variable for device ' + device)
-	os.environ['DISPLAY'] = ':0.' + device
-	log.debug('Confirming environment: ' + os.environ['DISPLAY'])
+	log = logging.getLogger('GPU%d-MINER' % device)
+	log.info('Worker starting on device %d', device)
+	log.debug('Setting DISPLAY variable for device %d', device)
+	os.environ['DISPLAY'] = ':0.%d' % device
+	log.debug('Confirming environment: %s', os.environ['DISPLAY'])
 	log.debug('Checking that device is found now that DISPLAY is set')
 
-	devices = cl.get_platforms()[0].platform.get_devices()
+	platform = cl.get_platforms()[0]
+	devices = platform.get_devices()
+
 
 	class StripMiner(BitcoinMiner):
 		def say(self, format, args=()):
-			log.info('Device[%s]: ' % device + format % args)
+			log.info(format % args)
 
 		def sayLine(self, format, args=()):
 			if(format == 'verification failed, check hardware!'):
-				message = 'Device[' + device + '] failed verification, check hardware!'
-				log.warn(message)
+				log.warn(format % args)
 			else:
 				self.say(format, args)
 
-		miner = StripMiner(device,
-					options.host,
-					options.user,
-					options.password,
-					options.port,
-					options.frames,
-					options.rate,
-					options.askrate,
-					options.worksize,
-					options.vectors,
-					options.verbose,
-					options.proxy)
-
+	try:
+		miner = StripMiner(devices[device],
+			options.host,
+			options.user,
+			options.password,
+			int(options.port),
+			int(options.frames),
+			float(options.rate),
+			int(options.askrate),
+			int(options.worksize),
+			bool(options.vectors),
+			bool(options.verbose))
 		miner.mine()
+	except:
+		log.info('Miner is exiting', device)
+	finally:
+		if miner: miner.exit()
 
 def main():
 	oParser = OptionParser(version=USER_AGENT,
@@ -76,6 +86,56 @@ def main():
 	if cParser.has_section('pool_%s' % options.pool):
 		for opt in cParser.options('pool_%s' % options.pool):
 			setattr(options, opt, cParser.get('pool_%s' % options.pool, opt))
+
+	# Setup logging if section is found in config
+	if options.logfile:
+		logfile = options.logfile
+		# logfile += '_' + strftime("%Y-%m-%d_%H%M%S")
+		logfile += '.log'
+		logging.basicConfig(filename=logfile, 
+			level=LOG_LEVEL, format=LOG_FORMAT, datefmt=LOG_DATEFMT)
+
+	log = logging.getLogger('stripminer')
+
+	# Collection GPU card information
+	log.info('Collecting available GPU information')
+	os.environ['DISPLAY'] = ':0'
+	devices = ADL.getNumGPU()
+
+	# Fire off mining workers for each device
+	processes = []
+	for device in devices:
+		gpuidx = device['GPU']
+		ADL.setIndex(gpuidx)
+		log.info('Device[GPU%d] %s (Core: %d, Memory: %d, Fan %d%%)',
+			gpuidx, device['Name'], ADL.getCoreClockSpeed(),
+			ADL.getMemoryClockSpeed(), ADL.getFanSpeed())
+		if cParser.has_section('ADL_GPU%d' % gpuidx):
+			log.debug('Found ADL settings for GPU%d', gpuidx)
+			if cParser.has_option('ADL_GPU%d' % gpuidx, 'core'):
+				core = cParser.getint('ADL_GPU%d' % gpuidx, 'core')
+				log.debug('Device[GPU%d] setting Core to %d Mhz', gpuidx, core)
+			if cParser.has_option('ADL_GPU%d' % gpuidx, 'memory'):
+				memory = cParser.getint('ADL_GPU%d' % gpuidx, 'memory')
+				log.debug('Device[GPU%d] setting Memory to %d Mhz', gpuidx, memory)
+			if cParser.has_option('ADL_GPU%d' % gpuidx, 'fan'):
+				fan = cParser.getint('ADL_GPU%d' % gpuidx, 'fan')
+				log.debug('Device[GPU%d] setting Fan to %d%%', gpuidx, fan)
+
+		p = Process(target=Miner, args=(gpuidx, options))
+		processes.append(p)
+
+	try:
+		for process in processes:
+			process.start()
+			process.join()
+	except KeyboardInterrupt:
+		log.info('Closing down the mine!')
+	finally:
+		for process in processes:
+			if process.is_alive():
+				process.terminate()
+				sleep(1.1)
 
 if __name__ == '__main__':
 	main()
